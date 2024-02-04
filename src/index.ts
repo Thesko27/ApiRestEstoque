60bbb514b868191d925806ad872b9fc9314ac28a
Thesko27 <franthesko1984@gmail.com>
2024-02-04 01:44:26 -0300

    feat: added  code style config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import {server} from './server/Server';
+import { server } from "./server/Server";
 
 
-server.listen(3333,()=>{
-console.log("Rodando na porta 3333");
-    
+server.listen(3333, () => {
+  console.log("Rodando na porta 3333");
+
 });
--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,11 +1,11 @@
-import express from 'express';
+import express from "express";
 
 
 const server = express();
 server.use(express.json());
 
-server.get('/', (_,res)=>{
-    return res.send('Welcome to the Home Page!');
-})
+server.get("/", (_, res) => {
+  return res.send("Welcome to the Home Page!");
+});
 
-export {server}
+export { server };
